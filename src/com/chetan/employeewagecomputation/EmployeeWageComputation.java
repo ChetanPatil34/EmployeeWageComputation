efefe695b1fdecf0d11660a722106dbe8b85c5cd
ChetanPatil34 <cp9210639@gmail.com>
2024-02-14 21:40:07 +0530

    [ADDED] : CHETAN Employee Attendance code

--- src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
@@ -1,10 +1,33 @@
 package com.chetan.employeewagecomputation;
 
+import java.util.Scanner;
+
+//import java.util.Random;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		int attendance ;
+		
+		System.out.print("Enter An attendence [0- Apsend / 1- Present] :");
+		Scanner obj = new Scanner(System.in);
+		attendance = obj.nextInt();
+		
+		if(attendance==0)
+		{
+			System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println("Employee is Present");
+		}
+		
+		
+		
+		
 	}
 
 }
