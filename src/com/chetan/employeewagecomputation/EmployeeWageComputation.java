389d2f861a24cd9f7713034618cb0f441ccef671
ChetanPatil34 <cp9210639@gmail.com>
2024-02-18 11:19:21 +0530

    [MODIFIED]:Chetan - UC-4 Completed for EmployeeType Calculate Wage and Attendence using switch case

--- src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
@@ -17,37 +17,46 @@ public class EmployeeWageComputation {
 	    final int Part_Time_Hour = 4;
 	    final int Full_Time_Hour = 8;
 	    
-		
-		System.out.print("Enter An attendence [0- Apsend / 1- Present] :");
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2;
 		
 		System.out.println(attendance);
 		
-		if(attendance==0)
+		switch(attendance)
 		{
+		    case 0 :
 			System.out.println("Employee is Absent");
-		}
-		else
-		{
-			System.out.println("Employee is Present");
-			
+			break;
+		
+		    case 1 :
+	        System.out.println("Employee is Present");
 			employee_Type = random.nextInt(9)%2;
 			
-			if(employee_Type==0)
-			{
-				System.out.println("Employee is Part Time");
+		        	switch(employee_Type)
+		        	{
+		        	
+		            	case 0 :
+			        	System.out.println("Employee is Part Time");
+				        dailyWage =  Wage_per_Hour * Part_Time_Hour ;
+			            System.out.println("Employee Wage :" + dailyWage);
 				
-				dailyWage =  Wage_per_Hour * Part_Time_Hour ;
-			}
-			else
-			{
-				System.out.println("Employee is Full Time");
-				
-				dailyWage =  Wage_per_Hour * Full_Time_Hour;
-				
-			}
-			System.out.println("Employee Wage :" + dailyWage);
+				        break;
+			  
+			           case 1 :
+			           System.out.println("Employee is Full Time");
+				       dailyWage =  Wage_per_Hour * Full_Time_Hour;
+				       System.out.println("Employee Wage :" + dailyWage);
+			
+				       break;
+			
+			          default:
+			        	System.out.println("Case isn't match");
+			        }
+			 break;
+
+		default:
+		System.out.println("Case isn't match");
+		}
 		
 		}
 		
@@ -55,4 +64,4 @@ public class EmployeeWageComputation {
 		
 	}
 
-}
+
