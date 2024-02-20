06046f9f1e72afcb91fe1409626e140e489a2e02
ChetanPatil34 <cp9210639@gmail.com>
2024-02-20 13:04:59 +0530

    [MODIFIED]:Chetan - UC-5 Refactored for Calculation of Wage for Month

--- src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/chetan/employeewagecomputation/EmployeeWageComputation.java
@@ -11,20 +11,26 @@ public class EmployeeWageComputation {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		int attendance ;
-		int dailyWage ;
+		int dailyWage = 0;
+		//int partTimer_dailyWage = 0;
+		//int fullTimer_dailyWage = 0;
 		int employee_Type;
+		int total_wage = 0;
+		int Employee_mouth_wage = 0;
 	    final int Wage_per_Hour = 20;
 	    final int Part_Time_Hour = 4;
 	    final int Full_Time_Hour = 8;
+	    final int WORKING_DAYS = 20;
 	    
+	    for(int i=1; i<=WORKING_DAYS; i++)
+	    {
+	    	System.out.println("Day :"+i);
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2;
 		
-		System.out.println(attendance);
-		
 		switch(attendance)
 		{
-		    case 0 :
+		   case 0 :
 			System.out.println("Employee is Absent");
 			break;
 		
@@ -35,33 +41,36 @@ public class EmployeeWageComputation {
 		        	switch(employee_Type)
 		        	{
 		        	
-		            	case 0 :
-			        	System.out.println("Employee is Part Time");
-				        dailyWage =  Wage_per_Hour * Part_Time_Hour ;
-			            System.out.println("Employee Wage :" + dailyWage);
-				
-				        break;
+		            	case 0:
+		            		System.out.println("Employee is Part Time");
+		            		dailyWage =  Wage_per_Hour * Part_Time_Hour ;
+		            		//System.out.println("Employee Wage :" + partTimer_dailyWage);
+		            		break;
 			  
-			           case 1 :
-			           System.out.println("Employee is Full Time");
-				       dailyWage =  Wage_per_Hour * Full_Time_Hour;
-				       System.out.println("Employee Wage :" + dailyWage);
-			
-				       break;
-			
-			          default:
-			        	System.out.println("Case isn't match");
-			        }
-			 break;
-
-		default:
-		System.out.println("Case isn't match");
-		}
+			           case 1:
+			        	   System.out.println("Employee is Full Time");
+			        	   dailyWage =  Wage_per_Hour * Full_Time_Hour;
+			        	   //System.out.println("Employee Wage :" + fullTimer_dailyWage);
+			        	   break;
+			        	   
+		        	}
+		        	 total_wage += dailyWage;
+				      System.out.println("Daily_wage="+dailyWage);
+		        	
+		            break;
+				}
 		
-		}
+			System.out.println("--------------------");
+	    }
+	
+	    System.out.println("Total Monthly="+total_wage);
+		     
+
+	}
+}
 		
 	
 		
-	}
+	
 
 
